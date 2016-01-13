6f7ead507194842f8f907d56ed94280ab7802ca8
Alex Sears <alexwsears@gmail.com>
2016-01-12 19:47:15 -0500

    webpack is setup?

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 var path = require('path');
 
+var WebpackNotifierPlugin = require('webpack-notifier');
+
 module.exports = {
     entry: path.resolve(__dirname, 'src/js/index.js'),
     output: {
         path: path.resolve(__dirname, 'build/js'),
         filename: 'bundle.js',
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                include: path.resolve(__dirname, 'src/js'),
+                exclude: /node_modules/,
+                loaders: ['eslint-loader'],
+            },
+        ],
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: path.resolve(__dirname, 'src/js'),
+                exclude: /node_modules/,
+                loaders: ['babel-loader'],
+            },
+        ],
+        plugins: [
+            new WebpackNotifierPlugin(),
+        ],
     },
 };
